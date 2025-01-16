fecd0267b7bc358e3605f5d52a1466918d2b6672
Sean Wilkinson <Sean.Wilkinson@morganwhite.com>
2025-01-16 09:37:00 -0600

    Restore deployment configuration to last working state: - Restore original web.config with fixed paths - Restore original install.cmd with wfastcgi setup - Simplify GitHub Actions workflow

--- install.cmd
+++ install.cmd
@@ -3,5 +3,8 @@ echo Installing dependencies...
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+echo Enabling wfastcgi...
+%SYSTEMDRIVE%\Python311\python.exe -m wfastcgi-enable
+
 echo Setup complete.
 exit /b 0
