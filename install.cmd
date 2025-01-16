194c2cd9e66d8307a03d95c651de703311e5f974
Sean Wilkinson <Sean.Wilkinson@morganwhite.com>
2025-01-16 09:32:39 -0600

    Fix deployment configuration: - Update web.config to use environment variables - Remove hardcoded Python paths from install.cmd - Improve GitHub Actions deployment process - Add proper deployment package creation

--- install.cmd
+++ install.cmd
@@ -3,8 +3,5 @@ echo Installing dependencies...
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-echo Enabling wfastcgi...
-%SYSTEMDRIVE%\Python311\python.exe -m wfastcgi-enable
-
 echo Setup complete.
 exit /b 0
