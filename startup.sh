d17feac1668b7372dcd727a25bcc95d16dc8b373
Sean Wilkinson <Sean.Wilkinson@morganwhite.com>
2025-01-17 11:01:49 -0600

    Update for Linux deployment: - Remove Windows-specific files - Add Linux startup script - Update dependencies for Linux - Configure gunicorn

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-python -m pip install --upgrade pip
+
+# Install ODBC Driver
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
+
+# Install dependencies
 pip install -r requirements.txt
-pip install pandas openpyxl
-gunicorn app:app
+
+# Start app with gunicorn
+gunicorn --bind=0.0.0.0:8000 app:app
