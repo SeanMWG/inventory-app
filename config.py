6c3b1455dc7a7889bb60bdd9d9a208cb5b60a609
Sean Wilkinson <Sean.Wilkinson@morganwhite.com>
2025-01-27 14:03:00 -0600

    Fix environment variable names
    - Update Azure AD config to use correct environment variable names:
      * CLIENT_ID instead of AZURE_CLIENT_ID
      * CLIENT_SECRET instead of AZURE_CLIENT_SECRET
      * TENANT_ID instead of AZURE_TENANT_ID

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 # Azure AD Configuration
-CLIENT_ID = os.getenv('AZURE_CLIENT_ID')
-CLIENT_SECRET = os.getenv('AZURE_CLIENT_SECRET')
-TENANT_ID = os.getenv('AZURE_TENANT_ID')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+TENANT_ID = os.environ.get('TENANT_ID')
 AUTHORITY = f'https://login.microsoftonline.com/{TENANT_ID}'
-REDIRECT_URI = os.getenv('AZURE_REDIRECT_URI', 'https://inventory-app-sean.azurewebsites.net/getAToken')
+REDIRECT_URI = os.environ.get('AZURE_REDIRECT_URI', 'https://inventory-app-sean.azurewebsites.net/getAToken')
 REDIRECT_PATH = '/getAToken'  # This matches the path in REDIRECT_URI
 SCOPE = ['User.Read']
 
 # Database Configuration
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
